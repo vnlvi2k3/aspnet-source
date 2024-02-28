680d3653ddfce95f0cdd42c8da2e735db564af07
LAPTOP-IGLVDQP8\Lan Vi <vi.vuvivu2203@hcmut.edu.vn>
2024-02-28 12:24:59 +0700

    init

---
+++ aspnet-core/CompanyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyCompanyName.AbpZeroTemplate.EntityFrameworkCore
+{
+	public class CompanyDbContext : AbpDbContext
+	{
+		public CompanyDbContext(DbContextOptions<CompanyDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
